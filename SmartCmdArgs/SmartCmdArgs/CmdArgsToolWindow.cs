c909ee2d9bfde2bfb0fb3729f186bb68864aebf9
MBulli <markusbullmann@gmail.com>
2015-09-23 22:51:21 +0200

    added blank tool window

---
+++ SmartCmdArgs/SmartCmdArgs/CmdArgsToolWindow.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="CmdArgsToolWindow.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace SmartCmdArgs
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("a21b35ed-5c13-4d55-a3d2-71054c4e9540")]
+    public class CmdArgsToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CmdArgsToolWindow"/> class.
+        /// </summary>
+        public CmdArgsToolWindow() : base(null)
+        {
+            this.Caption = "Smart Commandline Arguments";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new View.CmdArgsToolWindowControl();
+        }
+    }
+}
---
+++ SmartCmdArgs/SmartCmdArgs/CmdArgsToolWindowPackage.cs
@@ -0,0 +1,91 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="CmdArgsToolWindowPackage.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+
+namespace SmartCmdArgs
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideToolWindow(typeof(CmdArgsToolWindow))]
+    [Guid(CmdArgsToolWindowPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class CmdArgsToolWindowPackage : Package
+    {
+        /// <summary>
+        /// CmdArgsToolWindowPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "131b0c0a-5dd0-4680-b261-86ab5387b86e";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CmdArgsToolWindow"/> class.
+        /// </summary>
+        public CmdArgsToolWindowPackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+
+            // TODO add option keys to store custom data in suo file
+            // this.AddOptionKey("Test");
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            CmdArgsToolWindowCommand.Initialize(this);
+            base.Initialize();
+        }
+
+        protected override void OnLoadOptions(string key, Stream stream)
+        {
+            base.OnLoadOptions(key, stream);
+        }
+        protected override void OnSaveOptions(string key, Stream stream)
+        {
+            base.OnSaveOptions(key, stream);
+        }
+
+        
+
+        #endregion
+    }
+}
