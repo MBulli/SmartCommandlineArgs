92dcaec4b7286af8f6593dcf7729609bdcb89784
Irame <felix_keller@gmx.de>
2015-10-20 15:47:38 +0200

    Added messageBox if the Add button is hit but no project is found. Bug fixed

--- SmartCmdArgs/SmartCmdArgs/ViewModel/CmdArgsToolWindowViewModel.cs
+++ SmartCmdArgs/SmartCmdArgs/ViewModel/CmdArgsToolWindowViewModel.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using IramesYTD.Utils;
+using Microsoft.VisualStudio.Shell.Interop;
 using SmartCmdArgs.Model;
 
 namespace SmartCmdArgs.ViewModel
@@ -15,16 +17,18 @@ namespace SmartCmdArgs.ViewModel
         public RelayCommand AddEntryCommand { get { return addEntryCommand.Value; } }
         private Lazy<RelayCommand> addEntryCommand = new Lazy<RelayCommand>(
             () => new RelayCommand(
-                () => {
-                    // TODO: add warning message
+                () =>
+                {
                     if (CmdArgStorage.Instance.CurStartupProject != null)
                         CmdArgStorage.Instance.AddEntry(command: "", enabled: true);
+                    else
+                        MessageBox.Show("No startup project found!", "No startup project", MessageBoxButton.OK, MessageBoxImage.Warning);
                 })); 
 
         public RelayCommand<CmdArgItem> RemoveEntryCommand { get { return removeEntryCommand.Value; } }
         private Lazy<RelayCommand<CmdArgItem>> removeEntryCommand = new Lazy<RelayCommand<CmdArgItem>>(
             () => new RelayCommand<CmdArgItem>(
-                item => CmdArgStorage.Instance.RemoveEntryById(item.Id))); 
+                item => { if (item != null) CmdArgStorage.Instance.RemoveEntryById(item.Id); })); 
 
         public CmdArgsToolWindowViewModel()
         {
