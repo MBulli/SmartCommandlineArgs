6cfca09a684172822fa879fb4732aca0ecb66711
MBulli <markusbullmann@gmail.com>
2015-12-23 22:07:17 +0100

    Added ProvideBindingPath attribute to package

--- SmartCmdArgs/SmartCmdArgs/CmdArgsPackage.cs
+++ SmartCmdArgs/SmartCmdArgs/CmdArgsPackage.cs
@@ -43,6 +43,7 @@ namespace SmartCmdArgs
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideToolWindow(typeof(ToolWindow))]
+    [ProvideBindingPath]
     [Guid(CmdArgsPackage.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
     public sealed class CmdArgsPackage : Package
