7316eb8757bbd3f53fe108b4b819443fa6854998
MBulli <markusbullmann@gmail.com>
2016-06-10 23:05:42 +0200

    Added description for version control options

--- SmartCmdArgs/SmartCmdArgs/CmdArgsOptionPage.cs
+++ SmartCmdArgs/SmartCmdArgs/CmdArgsOptionPage.cs
@@ -11,8 +11,8 @@ namespace SmartCmdArgs
     public class CmdArgsOptionPage : DialogPage
     {
         [Category("General")]
-        [DisplayName("Source version control support")]
-        [Description("Lets you enable or disable svc support.")] // TODO wordly description
+        [DisplayName("Enable version control support")]
+        [Description("If enabled the extension will store the commandline arguments into an json file at the same loctation as the related project file. That way the commandline arguments might be version controlled by a VCS. If disabled the extension will store everything inside the solutions .suo-file which is usally ignored by version control. The default value for this setting is True.")]
         public bool SvcSupport { get; set; }
     }
 }
