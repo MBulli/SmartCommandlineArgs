0d354cfb01c814e39504d54b1c8770368f2a2bc9
MBulli <markusbullmann@gmail.com>
2016-06-30 21:59:40 +0200

    Added inital test project

---
+++ SmartCmdArgs/SmartCmdArgsTests/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Reflection;
+using Microsoft.VsSDK.UnitTestLibrary;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartCmdArgs;
+using Microsoft.VSSDK.Tools.VsIdeTesting;
+using EnvDTE;
+using Microsoft.VsSDK.IntegrationTestLibrary;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private DTE Dte => VsIdeTestHostContext.Dte;
+        private TestUtils Utils { get; } = new TestUtils();
+
+
+        private object InvokeInUIThread(Action method)
+            => UIThreadInvoker.Invoke(method);
+
+        private Interface GetService<Service, Interface>()
+            where Interface : class
+        {
+            return VsIdeTestHostContext.ServiceProvider.GetService(typeof(Service)) as Interface;
+        }
+
+        [TestMethod]
+        [HostType("VS IDE")]
+        [TestProperty("VsHiveName", "14.0Exp")]
+        public void TestMethod1()
+        {
+            InvokeInUIThread(() => {
+                IVsShell shell = GetService<SVsShell, IVsShell>();
+                Assert.IsNotNull(shell);
+
+                IVsPackage package = Utils.LoadPackage(new Guid(SmartCmdArgs.CmdArgsPackage.PackageGuidString));
+            });
+        }
+    }
+}
