da64b35798d7ef3df6eba0aa829b012952187e32
Irame <irame94@gmail.com>
2016-07-01 03:14:39 +0200

    Added first "real" test

--- SmartCmdArgs/SmartCmdArgsTests/UnitTest1.cs
+++ SmartCmdArgs/SmartCmdArgsTests/UnitTest1.cs
@@ -1,16 +1,15 @@
 ﻿using System;
-using System.Collections;
-using System.Text;
-using System.Reflection;
-using Microsoft.VsSDK.UnitTestLibrary;
+using System.IO;
+using System.Text.RegularExpressions;
+using EnvDTE;
+using EnvDTE80;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SmartCmdArgs;
-using Microsoft.VSSDK.Tools.VsIdeTesting;
-using EnvDTE;
 using Microsoft.VsSDK.IntegrationTestLibrary;
+using Microsoft.VSSDK.Tools.VsIdeTesting;
+using SmartCmdArgs;
 
-namespace UnitTestProject1
+namespace SmartCmdArgsTests
 {
     [TestClass]
     public class UnitTest1
@@ -31,14 +30,38 @@ namespace UnitTestProject1
         [TestMethod]
         [HostType("VS IDE")]
         [TestProperty("VsHiveName", "14.0Exp")]
-        public void TestMethod1()
+        public void CollectArgsFromExistingProjectConfigsTest()
         {
-            InvokeInUIThread(() => {
-                IVsShell shell = GetService<SVsShell, IVsShell>();
-                Assert.IsNotNull(shell);
+            string rootPath = Directory.GetCurrentDirectory();
+            string solutionName = "TestSolution";
+            string projectName = "TestProject";
 
-                IVsPackage package = Utils.LoadPackage(new Guid(SmartCmdArgs.CmdArgsPackage.PackageGuidString));
-            });
+            string solutionPath = Path.Combine(rootPath, solutionName);
+            string projectPath = Path.Combine(solutionPath, projectName);
+
+            Solution solution = Dte.Solution;
+
+            solution.Create(solutionPath, solutionName);
+
+            Solution2 solution2 = (Solution2)solution;
+            var projectTemplate = solution2.GetProjectTemplate("ConsoleApplication.zip", "CSharp");
+
+            solution.AddFromTemplate(projectTemplate, projectPath, projectName);
+            Project project = solution.Projects.Item(1);
+
+            EnvDTE.Properties properties = project.ConfigurationManager?.ActiveConfiguration?.Properties;
+            properties.Item("StartArguments").Value = "bla lol 3.Arg";
+
+            IVsPackage package = Utils.LoadPackage(new Guid(CmdArgsPackage.PackageGuidString));
+            Assert.IsNotNull(package);
+
+            solution.SaveAs(Path.Combine(solutionPath, solutionName + ".sln"));
+
+            string jsonContent = File.ReadAllText(Path.Combine(projectPath, projectName + ".args.json"));
+
+            Regex jsonRegex = new Regex("\\{\\s*\"DataCollection\":\\s*\\[\\s*\\{\\s*\"Id\":\\s*\"[^\"]*\",\\s*\"Command\":\\s*\"bla lol 3\\.Arg\"\\s*\\}\\s*\\]\\s*\\}");
+
+            Assert.IsTrue(jsonRegex.IsMatch(jsonContent));
         }
     }
 }
