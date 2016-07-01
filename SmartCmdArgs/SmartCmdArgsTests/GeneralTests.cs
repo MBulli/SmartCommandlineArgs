4994c52a70a5fb95de260163a870af7f448646af
Irame <irame94@gmail.com>
2016-07-01 19:29:16 +0200

    reoranized test classes

--- SmartCmdArgs/SmartCmdArgsTests/GeneralTests.cs
+++ SmartCmdArgs/SmartCmdArgsTests/GeneralTests.cs
@@ -16,22 +16,8 @@ using SmartCmdArgs;
 namespace SmartCmdArgsTests
 {
     [TestClass]
-    public class UnitTest1
+    public class GeneralTests : TestBase
     {
-        private DTE Dte => VsIdeTestHostContext.Dte;
-        private TestUtils Utils { get; } = new TestUtils();
-
-        private string RootDir => Directory.GetCurrentDirectory();
-
-        private object InvokeInUIThread(Action method)
-            => UIThreadInvoker.Invoke(method);
-
-        private Interface GetService<Service, Interface>()
-            where Interface : class
-        {
-            return VsIdeTestHostContext.ServiceProvider.GetService(typeof(Service)) as Interface;
-        }
-
         [TestMethod]
         [HostType("VS IDE")]
         [TestProperty(VsIdeTestHostContants.TestPropertyName.RegistryHiveName, "14.0Exp")]
@@ -118,92 +104,6 @@ namespace SmartCmdArgsTests
                 Assert.AreNotEqual(Guid.Empty, argItem.Id);
                 Assert.AreEqual("", argItem.Command);
             });
-        }
-
-        [TestMethod]
-        [HostType("VS IDE")]
-        [TestProperty(VsIdeTestHostContants.TestPropertyName.RegistryHiveName, "14.0Exp")]
-        [TestProperty(VsIdeTestHostContants.TestPropertyName.RestartOptions, VsIdeTestHostContants.HostRestartOptions.Before)]
-        public void SaveCommandsToJsonTest()
-        {
-            string solutionName = "SaveCommandsToJsonTestSolution";
-            string projectName = "SaveCommandsToJsonTestProject";
-            CreateSolutionWithProject(solutionName, projectName);
-
-            var package = (CmdArgsPackage)Utils.LoadPackage(new Guid(CmdArgsPackage.PackageGuidString));
-
-            InvokeInUIThread(() =>
-            {
-                var curList = package?.ToolWindowViewModel?.CurrentArgumentList;
-                Assert.IsNotNull(curList);
-
-                var initalCommands = new[] {"arg1", "Arg2", "arg 3"};
-
-                foreach (var initalCommand in initalCommands)
-                {
-                    package.ToolWindowViewModel.CurrentArgumentList.AddNewItem(initalCommand);
-                }
-
-                Utils.ForceSaveSolution();
-
-                string jsonFile = Path.Combine(Path.Combine(Path.Combine(
-                        RootDir,
-                        solutionName),
-                        projectName),
-                        projectName + ".args.json");
-
-                CheckJsonFile(jsonFile, initalCommands);
-            });
-        }
-
-        private Project CreateSolutionWithProject(string solutionName = "TestSolution", string projectName = "TestProject")
-        {
-            Debug.WriteLine("CreateSolutionWithProject");
-
-            string solutionPath = Path.Combine(RootDir, solutionName);
-            string projectPath = Path.Combine(solutionPath, projectName);
-
-            if(Directory.Exists(solutionPath)) Directory.Delete(solutionPath, true);
-
-            Solution2 solution = (Solution2)Dte.Solution;
-
-            solution.Create(solutionPath, solutionName);
-
-            var projectTemplate = solution.GetProjectTemplate("ConsoleApplication.zip", "CSharp");
-
-            solution.AddFromTemplate(projectTemplate, projectPath, projectName);
-            return solution.Projects.Item(1);
-        }
-
-        private void CheckJsonFile(string jsonFile, string[] commands)
-        {
-            var jsonRegex = new Regex("\\{\\s*\"DataCollection\":\\s*\\[(?:\\s*\\{\\s*\"Id\":\\s*\"(?<id>[^\"]*)\",\\s*\"Command\":\\s*\"(?<command>.*?)\"\\s*\\}\\s*,?)*\\s*\\]\\s*\\}", RegexOptions.Compiled);
-
-            Assert.IsTrue(File.Exists(jsonFile));
-
-            string jsonFileContent = File.ReadAllText(jsonFile);
-
-            var match = jsonRegex.Match(jsonFileContent);
-
-            Assert.IsNotNull(match);
-            Assert.IsTrue(match.Success);
-
-            var mathedIds = match.Groups["id"]?.Captures;
-            var matchedCommands = match.Groups["command"]?.Captures;
-
-            Assert.IsNotNull(mathedIds);
-            Assert.IsNotNull(matchedCommands);
-            Assert.AreEqual(commands.Length, mathedIds.Count);
-            Assert.AreEqual(commands.Length, matchedCommands.Count);
-
-            for (int i = 0; i < 3; i++)
-            {
-                Assert.IsFalse(string.IsNullOrEmpty(mathedIds[i].Value));
-                Assert.AreNotEqual(Guid.Empty.ToString(), mathedIds[i].Value);
-
-                Assert.IsFalse(string.IsNullOrEmpty(matchedCommands[i].Value));
-                Assert.AreEqual(commands[i], matchedCommands[i].Value);
-            }
         }
     }
 }
---
+++ SmartCmdArgs/SmartCmdArgsTests/VcsSupportTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VSSDK.Tools.VsIdeTesting;
+using SmartCmdArgs;
+
+namespace SmartCmdArgsTests
+{
+    [TestClass]
+    public class VcsSupportTests : TestBase
+    {
+        [TestMethod]
+        [HostType("VS IDE")]
+        [TestProperty(VsIdeTestHostContants.TestPropertyName.RegistryHiveName, "14.0Exp")]
+        [TestProperty(VsIdeTestHostContants.TestPropertyName.RestartOptions, VsIdeTestHostContants.HostRestartOptions.Before)]
+        public void SaveCommandsToJsonTest()
+        {
+            string solutionName = "SaveCommandsToJsonTestSolution";
+            string projectName = "SaveCommandsToJsonTestProject";
+            CreateSolutionWithProject(solutionName, projectName);
+
+            var package = (CmdArgsPackage)Utils.LoadPackage(new Guid(CmdArgsPackage.PackageGuidString));
+
+            InvokeInUIThread(() =>
+            {
+                var curList = package?.ToolWindowViewModel?.CurrentArgumentList;
+                Assert.IsNotNull(curList);
+
+                var initalCommands = new[] { "arg1", "Arg2", "arg 3" };
+
+                foreach (var initalCommand in initalCommands)
+                {
+                    package.ToolWindowViewModel.CurrentArgumentList.AddNewItem(initalCommand);
+                }
+
+                Utils.ForceSaveSolution();
+
+                string jsonFile = Path.Combine(Path.Combine(Path.Combine(
+                        RootDir,
+                        solutionName),
+                        projectName),
+                        projectName + ".args.json");
+
+                CheckJsonFile(jsonFile, initalCommands);
+            });
+        }
+
+
+        private void CheckJsonFile(string jsonFile, string[] commands)
+        {
+            var jsonRegex = new Regex("\\{\\s*\"DataCollection\":\\s*\\[(?:\\s*\\{\\s*\"Id\":\\s*\"(?<id>[^\"]*)\",\\s*\"Command\":\\s*\"(?<command>.*?)\"\\s*\\}\\s*,?)*\\s*\\]\\s*\\}", RegexOptions.Compiled);
+
+            Assert.IsTrue(File.Exists(jsonFile));
+
+            string jsonFileContent = File.ReadAllText(jsonFile);
+
+            var match = jsonRegex.Match(jsonFileContent);
+
+            Assert.IsNotNull(match);
+            Assert.IsTrue(match.Success);
+
+            var mathedIds = match.Groups["id"]?.Captures;
+            var matchedCommands = match.Groups["command"]?.Captures;
+
+            Assert.IsNotNull(mathedIds);
+            Assert.IsNotNull(matchedCommands);
+            Assert.AreEqual(commands.Length, mathedIds.Count);
+            Assert.AreEqual(commands.Length, matchedCommands.Count);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.IsFalse(string.IsNullOrEmpty(mathedIds[i].Value));
+                Assert.AreNotEqual(Guid.Empty.ToString(), mathedIds[i].Value);
+
+                Assert.IsFalse(string.IsNullOrEmpty(matchedCommands[i].Value));
+                Assert.AreEqual(commands[i], matchedCommands[i].Value);
+            }
+        }
+    }
+}
