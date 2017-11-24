93ffef7f62a7449724caa7aa5513cf87e373ea5c
Irame <irame94@gmail.com>
2017-11-24 23:38:48 +0100

    adjusted colors and some ui behavior

---
+++ SmartCmdArgs/SmartCmdArgs/View/Converter/EmptyToVisibilityConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCmdArgs.View.Converter
+{
+    class EmptyToVisibilityConverter : ConverterBase
+    {
+        public EmptyToVisibilityConverter()
+        {
+        }
+
+        public bool Inverted
+        {
+            get;
+            set;
+        }
+
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool empty = false;
+            if (value is ICollection collection && collection.Count == 0)
+                empty = true;
+            else if (value is IEnumerable enumerable && !enumerable.GetEnumerator().MoveNext())
+                empty = true;
+
+            if (Inverted)
+                return !empty ? System.Windows.Visibility.Hidden : System.Windows.Visibility.Visible;
+            else
+                return empty ? System.Windows.Visibility.Hidden : System.Windows.Visibility.Visible;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
