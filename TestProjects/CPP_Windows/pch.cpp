c4a914f6961afaf57092b5e784eeef524a9c82b7
Irame <irame94@gmail.com>
2019-02-19 21:48:09 +0100

    Added Test Projects for supported Languages.

---
+++ TestProjects/CPP_Windows/pch.cpp
@@ -0,0 +1,5 @@
+// pch.cpp: Quelldatei, die dem vorkompilierten Header entspricht. Diese ist für eine erfolgreiche Kompilierung erforderlich.
+
+#include "pch.h"
+
+// Im Allgemeinen ignorieren Sie diese Datei, aber heben sie auf, wenn Sie vorkompilierte Header verwenden.
---
+++ TestProjects/CS_DotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_DotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
