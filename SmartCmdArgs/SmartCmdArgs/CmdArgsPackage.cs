309c61e761b1652b70d3496afc930bdbb840ff92
MBulli <mbulli135@gmail.com>
2019-03-28 21:00:46 +0100

    Pushed version to 2.1.1

--- SmartCmdArgs/SmartCmdArgs/CmdArgsPackage.cs
+++ SmartCmdArgs/SmartCmdArgs/CmdArgsPackage.cs
@@ -51,7 +51,7 @@ namespace SmartCmdArgs
     /// </para>
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [InstalledProductRegistration("#110", "#112", "2.1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [InstalledProductRegistration("#110", "#112", "2.1.1", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideToolWindow(typeof(ToolWindow), Window = ToolWindow.ToolWindowGuidString)]
     [ProvideOptionPage(typeof(CmdArgsOptionPage), "Smart Command Line Arguments", "General", 1000, 1001, false)]   
