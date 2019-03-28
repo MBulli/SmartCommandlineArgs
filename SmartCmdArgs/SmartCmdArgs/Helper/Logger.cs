73712688b16e74202a551d2556e30c3d7eada8b9
MBulli <mbulli135@gmail.com>
2019-03-28 20:59:41 +0100

    Added thread ID to log messages

--- SmartCmdArgs/SmartCmdArgs/Helper/Logger.cs
+++ SmartCmdArgs/SmartCmdArgs/Helper/Logger.cs
@@ -14,26 +14,31 @@ namespace SmartCmdArgs.Helper
 
         public static void Info(string message)
         {
-            ActivityLog.TryLogInformation(LogSource, message);
+            ActivityLog.TryLogInformation(LogSource, PrependThreadId(message));
             WriteToDebugConsole("Info", message);
         }
 
         public static void Warn(string message)
         {
-            ActivityLog.TryLogWarning(LogSource, message);
+            ActivityLog.TryLogWarning(LogSource, PrependThreadId(message));
             WriteToDebugConsole("Warning", message);
         }
 
         public static void Error(string message)
         {
-            ActivityLog.TryLogError(LogSource, message);
+            ActivityLog.TryLogError(LogSource, PrependThreadId(message));
             WriteToDebugConsole("Error", message);
         }
 
         [Conditional("DEBUG")]
         private static void WriteToDebugConsole(string type, string message)
         {
-            Debug.WriteLine($"{LogSource}|{type}: {message}");
+            Debug.WriteLine($"{LogSource}[{System.Threading.Thread.CurrentThread.ManagedThreadId}]|{type}: {message}");
+        }
+
+        private static string PrependThreadId(string message)
+        {
+            return $"[{System.Threading.Thread.CurrentThread.ManagedThreadId}] {message}";
         }
     }
 }
