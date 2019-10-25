f1a0216b10827a8ee7fba8be619fdbfe8e5b63da
MBulli <mbulli135@gmail.com>
2019-10-25 19:55:34 +0200

    Refactored file storage related code into own class as preparation for use solution dir mode (Issue #88)

--- SmartCmdArgs/SmartCmdArgs/CmdArgsPackage.cs
+++ SmartCmdArgs/SmartCmdArgs/CmdArgsPackage.cs
@@ -73,11 +73,12 @@ namespace SmartCmdArgs
         private readonly Regex msBuildPropertyRegex = new Regex(@"\$\((?<propertyName>(?:(?!\$\()[^)])*?)\)", RegexOptions.Compiled);
 
         private VisualStudioHelper vsHelper;
+        private FileStorage fileStorage;
         public ToolWindowViewModel ToolWindowViewModel { get; }
 
         public static CmdArgsPackage Instance { get; private set; }
 
-        private bool IsVcsSupportEnabled => GetDialogPage<CmdArgsOptionPage>().VcsSupport;
+        public bool IsVcsSupportEnabled => GetDialogPage<CmdArgsOptionPage>().VcsSupport;
         private bool IsMacroEvaluationEnabled => GetDialogPage<CmdArgsOptionPage>().MacroEvaluation;
         private bool IsUseMonospaceFontEnabled => GetDialogPage<CmdArgsOptionPage>().UseMonospaceFont;
         private bool IsUseSolutionDirEnabled => GetDialogPage<CmdArgsOptionPage>().UseSolutionDir; 
@@ -89,9 +90,7 @@ namespace SmartCmdArgs
         // processed later.
         private string toolWindowStateFromSolutionJsonStr;
         private ToolWindowStateSolutionData toolWindowStateLoadedFromSolution;
-
-        private Dictionary<Guid, FileSystemWatcher> projectFsWatchers = new Dictionary<Guid, FileSystemWatcher>();
-
+        
         /// <summary>
         /// Initializes a new instance of the <see cref="ToolWindow"/> class.
         /// </summary>
@@ -145,6 +144,9 @@ namespace SmartCmdArgs
             vsHelper.ProjectBeforeClose += VsHelper_ProjectRemoved;
             vsHelper.ProjectAfterRename += VsHelper_ProjectRenamed;
 
+            fileStorage = new FileStorage(this, vsHelper);
+            fileStorage.FileStorageChanged += FileStorage_FileStorageChanged;
+
             await vsHelper.InitializeAsync();
 
             // Switch to main thread
@@ -181,7 +183,7 @@ namespace SmartCmdArgs
                 try
                 {
                     var project = vsHelper.HierarchyForProjectGuid(projectGuid);
-                    SaveJsonForProject(project);
+                    fileStorage.SaveProject(project);
                 }
                 catch(Exception ex)
                 {
@@ -224,58 +226,6 @@ namespace SmartCmdArgs
                 Logger.Info("Saving commands to suo file.");
                 toolWindowStateLoadedFromSolution = ToolWindowSolutionDataSerializer.Serialize(ToolWindowViewModel, stream);
                 Logger.Info("All Commands saved to suo file.");
-            }
-        }
-
-        private void SaveJsonForProject(IVsHierarchy project)
-        {
-            if (!IsVcsSupportEnabled || project == null)
-                return;
-
-            var guid = project.GetGuid();
-            var vm = ToolWindowViewModel.TreeViewModel.Projects.GetValueOrDefault(guid);
-            string filePath = FullFilenameForProjectJsonFileFromProject(project);
-            FileSystemWatcher fsWatcher = projectFsWatchers.GetValueOrDefault(guid);
-
-            if (vm != null && vm.Items.Any())
-            {
-                using (fsWatcher?.TemporarilyDisable())
-                {
-                    // Tell VS that we're about to change this file
-                    // This matters if the user has TFVC with server workpace (see #57)
-
-                    if (!vsHelper.CanEditFile(filePath))
-                    {
-                        Logger.Error($"VS or the user did no let us editing our file :/");
-                    }
-                    else
-                    {
-                        try
-                        {
-                            using (Stream fileStream = File.Open(filePath, FileMode.Create, FileAccess.Write))
-                            {
-                                Logic.ToolWindowProjectDataSerializer.Serialize(vm, fileStream);
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            Logger.Warn($"Failed to write to file '{filePath}' with error '{e}'.");
-                        }
-                    }
-                }
-            }
-            else if (File.Exists(filePath))
-            {
-                Logger.Info("Deleting json file because command list is empty but json-file exists.");
-
-                try
-                {
-                    File.Delete(filePath);
-                }
-                catch (Exception e)
-                {
-                    Logger.Warn($"Failed to delete file '{filePath}' with error '{e}'.");
-                }
             }
         }
 
@@ -359,61 +309,10 @@ namespace SmartCmdArgs
             return launchProfiles ?? new List<string>();
         }
 
-        private void AttachFsWatcherToProject(IVsHierarchy project)
+        private void FileStorage_FileStorageChanged(object sender, FileStorageChangedEventArgs e)
         {
-            string realProjectJsonFileFullName = SymbolicLinkUtils.GetRealPath(FullFilenameForProjectJsonFileFromProject(project));
-            try
-            {
-                var projectJsonFileWatcher = new FileSystemWatcher();
-
-                projectJsonFileWatcher.Path = Path.GetDirectoryName(realProjectJsonFileFullName);
-                projectJsonFileWatcher.Filter = Path.GetFileName(realProjectJsonFileFullName);
-
-                projectJsonFileWatcher.EnableRaisingEvents = true;
-                projectFsWatchers.Add(project.GetGuid(), projectJsonFileWatcher);
-
-                projectJsonFileWatcher.Changed += (fsWatcher, args) => {
-                    Logger.Info($"SystemFileWatcher file Change '{args.FullPath}'");
-                    UpdateCommandsForProjectOnDispatcher(project, onlyIfVcsSupportEnabled: true);
-                };
-                projectJsonFileWatcher.Created += (fsWatcher, args) => {
-                    Logger.Info($"SystemFileWatcher file Created '{args.FullPath}'");
-                    UpdateCommandsForProjectOnDispatcher(project, onlyIfVcsSupportEnabled: true);
-                };
-                projectJsonFileWatcher.Renamed += (fsWatcher, args) =>
-                {
-                    Logger.Info($"FileWachter file Renamed '{args.FullPath}'. realProjectJsonFileFullName='{realProjectJsonFileFullName}'");
-                    if (realProjectJsonFileFullName == args.FullPath)
-                        UpdateCommandsForProjectOnDispatcher(project, onlyIfVcsSupportEnabled: true);
-                };
-
-                Logger.Info($"Attached FileSystemWatcher to file '{realProjectJsonFileFullName}' for project '{project.GetName()}'.");
-            }
-            catch (Exception e)
-            {
-                Logger.Warn($"Failed to attach FileSystemWatcher to file '{realProjectJsonFileFullName}' for project '{project.GetName()}' with error '{e}'.");
-            }
-        }
-
-        private void DetachFsWatcherFromAllProjects()
-        {
-            foreach (var projectFsWatcher in projectFsWatchers)
-            {
-                projectFsWatcher.Value.Dispose();
-                Logger.Info($"Detached FileSystemWatcher for project '{projectFsWatcher.Key}'.");
-            }
-            projectFsWatchers.Clear();
-        }
-
-        private void DetachFsWatcherFromProject(IVsHierarchy project)
-        {
-            var guid = project.GetGuid();
-            if (projectFsWatchers.TryGetValue(guid, out FileSystemWatcher fsWatcher))
-            {
-                fsWatcher.Dispose();
-                projectFsWatchers.Remove(guid);
-                Logger.Info($"Detached FileSystemWatcher for project '{project.GetName()}'.");
-            }
+            // This event is triggered on non-main thread!
+            UpdateCommandsForProjectOnDispatcher(e.Project, onlyIfVcsSupportEnabled:true);
         }
 
         private void UpdateCommandsForProjectOnDispatcher(IVsHierarchy project, bool onlyIfVcsSupportEnabled)
@@ -476,28 +375,7 @@ namespace SmartCmdArgs
             ToolWindowStateProjectData projectData = null;
             if (IsVcsSupportEnabled)
             {
-                string filePath = FullFilenameForProjectJsonFileFromProject(project);
-
-                if (File.Exists(filePath))
-                {
-                    try
-                    {
-                        using (Stream fileStream = File.Open(filePath, FileMode.Open, FileAccess.Read))
-                        {
-                            projectData = Logic.ToolWindowProjectDataSerializer.Deserialize(fileStream);
-                        }
-                        Logger.Info($"Read {projectData?.Items?.Count} commands for project '{project.GetName()}' from json-file '{filePath}'.");
-                    }
-                    catch (Exception e)
-                    {
-                        Logger.Warn($"Failed to read file '{filePath}' with error '{e}'.");
-                        projectData = null;
-                    }
-                }
-                else
-                {
-                    Logger.Info($"Json-file '{filePath}' doesn't exists.");
-                }
+                projectData = fileStorage.ReadDataForProject(project);
             }
 
             // project json overrides if it exists
@@ -615,7 +493,7 @@ namespace SmartCmdArgs
             foreach (var project in vsHelper.GetSupportedProjects())
             {
                 UpdateCommandsForProject(project);
-                AttachFsWatcherToProject(project);
+                fileStorage.AddProject(project);
             }
             UpdateCurrentStartupProject();
         }
@@ -632,7 +510,7 @@ namespace SmartCmdArgs
         {
             Logger.Info("VS-Event: Solution will close.");
 
-            DetachFsWatcherFromAllProjects();
+            fileStorage.RemoveAllProjects();
         }
 
         private void VsHelper_SolutionClosed(object sender, EventArgs e)
@@ -663,7 +541,7 @@ namespace SmartCmdArgs
             {
                 var project = vsHelper.HierarchyForProjectGuid(startupProject.Id);
                 UpdateConfigurationForProject(project);
-                SaveJsonForProject(project);
+                fileStorage.SaveProject(project);
             }
         }
 
@@ -677,7 +555,7 @@ namespace SmartCmdArgs
             ToolWindowHistory.SaveState();
 
             UpdateCommandsForProject(e.Project);
-            AttachFsWatcherToProject(e.Project);
+            fileStorage.AddProject(e.Project);
         }
 
         private void VsHelper_ProjectRemoved(object sender, VisualStudioHelper.ProjectBeforeCloseEventArgs e)
@@ -687,44 +565,24 @@ namespace SmartCmdArgs
             if (e.IsSolutionCloseProcess)
                 return;
 
-            SaveJsonForProject(e.Project);
+            fileStorage.SaveProject(e.Project);
 
             ToolWindowViewModel.TreeViewModel.Projects.Remove(e.Project.GetGuid());
 
-            DetachFsWatcherFromProject(e.Project);
+            fileStorage.RemoveProject(e.Project);
         }
 
         private void VsHelper_ProjectRenamed(object sender, VisualStudioHelper.ProjectAfterRenameEventArgs e)
         {
             Logger.Info($"VS-Event: Project '{e.OldProjectName}' renamed to '{e.Project.GetName()}'.");
 
-            var guid = e.Project.GetGuid();
-            if (projectFsWatchers.TryGetValue(guid, out FileSystemWatcher fsWatcher))
+            fileStorage.RenameProject(e.Project, e.OldProjectDir, e.OldProjectName, () =>
             {
-                projectFsWatchers.Remove(guid);
-                using (fsWatcher.TemporarilyDisable())
-                {
-                    var newFileName = FullFilenameForProjectJsonFileFromProject(e.Project);
-                    var oldFileName = FullFilenameForProjectJsonFileFromProjectPath(e.OldProjectDir, e.OldProjectName);
+                ToolWindowHistory.SaveState();
 
-                    Logger.Info($"Renaming json-file '{oldFileName}' to new name '{newFileName}'");
+                UpdateCommandsForProject(e.Project);
+            });
 
-                    if (File.Exists(newFileName))
-                    {
-                        File.Delete(oldFileName);
-
-                        ToolWindowHistory.SaveState();
-
-                        UpdateCommandsForProject(e.Project);
-                    }
-                    else if (File.Exists(oldFileName))
-                    {
-                        File.Move(oldFileName, newFileName);
-                    }
-                    fsWatcher.Filter = Path.GetFileName(newFileName);
-                }
-                projectFsWatchers.Add(guid, fsWatcher);
-            }
             ToolWindowViewModel.RenameProject(e.Project);
         }
         #endregion
@@ -769,30 +627,6 @@ namespace SmartCmdArgs
 
             ToolWindowViewModel.TreeViewModel.Projects.ForEach(p => p.Value.IsStartupProject = startupProjectGuids.Contains(p.Key));
             ToolWindowViewModel.TreeViewModel.UpdateTree();
-        }
-
-        private string FullFilenameForProjectJsonFileFromProject(IVsHierarchy project)
-        {
-            var userFilename = vsHelper.GetMSBuildPropertyValue(project, "SmartCmdArgJsonFile");
-            
-            if (!string.IsNullOrEmpty(userFilename))
-            {
-                // It's recommended to use absolute paths for the json file in the first place...
-                userFilename = Path.GetFullPath(userFilename); // ... but make it absolute in any case.
-                
-                Logger.Info($"'SmartCmdArgJsonFile' msbuild property present in project '{project.GetName()}' will use json file '{userFilename}'.");
-                return userFilename;
-            }
-            else
-            {
-                return FullFilenameForProjectJsonFileFromProjectPath(project.GetProjectDir(), project.GetName());
-            }
-        }
-
-        private string FullFilenameForProjectJsonFileFromProjectPath(string projectDir, string projectName)
-        {
-            string filename = $"{projectName}.args.json";
-            return Path.Combine(projectDir, filename);
         }
     }
 }
---
+++ SmartCmdArgs/SmartCmdArgs/Logic/FileStorage.cs
@@ -0,0 +1,261 @@
+﻿using Microsoft.VisualStudio.Shell.Interop;
+using SmartCmdArgs.Helper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCmdArgs.Logic
+{
+    /// <summary>
+    /// This class handles all the file related functionality
+    /// </summary>
+    class FileStorage
+    {
+        private readonly CmdArgsPackage cmdPackage;
+        private readonly VisualStudioHelper vsHelper;
+
+        private Dictionary<Guid, FileSystemWatcher> projectFsWatchers = new Dictionary<Guid, FileSystemWatcher>();
+
+        public event EventHandler<FileStorageChangedEventArgs> FileStorageChanged;
+
+        public FileStorage(CmdArgsPackage cmdPackage, VisualStudioHelper vsHelper)
+        {
+            this.cmdPackage = cmdPackage;
+            this.vsHelper = vsHelper;
+        }
+
+        public void AddProject(IVsHierarchy project)
+        {
+            AttachFsWatcherToProject(project);
+        }
+
+        public void RemoveAllProjects()
+        {
+            DetachFsWatcherFromAllProjects();
+        }
+
+        public void RemoveProject(IVsHierarchy project)
+        {
+            DetachFsWatcherFromProject(project);
+        }
+
+        public void RenameProject(IVsHierarchy project, string oldProjectDir, string oldProjectName, Action hack)
+        {
+            var guid = project.GetGuid();
+            if (projectFsWatchers.TryGetValue(guid, out FileSystemWatcher fsWatcher))
+            {
+                projectFsWatchers.Remove(guid);
+                using (fsWatcher.TemporarilyDisable())
+                {
+                    var newFileName = FullFilenameForProjectJsonFileFromProject(project);
+                    var oldFileName = FullFilenameForProjectJsonFileFromProjectPath(oldProjectDir, oldProjectName);
+
+                    Logger.Info($"Renaming json-file '{oldFileName}' to new name '{newFileName}'");
+
+                    if (File.Exists(newFileName))
+                    {
+                        File.Delete(oldFileName);
+
+                        hack(); // TODO
+                    }
+                    else if (File.Exists(oldFileName))
+                    {
+                        File.Move(oldFileName, newFileName);
+                    }
+                    fsWatcher.Filter = Path.GetFileName(newFileName);
+                }
+                projectFsWatchers.Add(guid, fsWatcher);
+            }
+        }
+
+        public ToolWindowStateProjectData ReadDataForProject(IVsHierarchy project)
+        {
+            ToolWindowStateProjectData result = null;
+
+            string filePath = FullFilenameForProjectJsonFileFromProject(project);
+
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    using (Stream fileStream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+                    {
+                        result = Logic.ToolWindowProjectDataSerializer.Deserialize(fileStream);
+                    }
+                    Logger.Info($"Read {result?.Items?.Count} commands for project '{project.GetName()}' from json-file '{filePath}'.");
+                }
+                catch (Exception e)
+                {
+                    Logger.Warn($"Failed to read file '{filePath}' with error '{e}'.");
+                    result = null;
+                }
+            }
+            else
+            {
+                Logger.Info($"Json-file '{filePath}' doesn't exists.");
+            }
+
+            return result;
+        }
+
+        public void SaveProject(IVsHierarchy project)
+        {
+            SaveJsonForProject(project);
+        }
+
+        private void SaveJsonForProject(IVsHierarchy project)
+        {
+            if (!cmdPackage.IsVcsSupportEnabled || project == null)
+                return;
+
+            var guid = project.GetGuid();
+            var vm = cmdPackage.ToolWindowViewModel.TreeViewModel.Projects.GetValueOrDefault(guid);
+            string filePath = FullFilenameForProjectJsonFileFromProject(project);
+            FileSystemWatcher fsWatcher = projectFsWatchers.GetValueOrDefault(guid);
+
+            if (vm != null && vm.Items.Any())
+            {
+                using (fsWatcher?.TemporarilyDisable())
+                {
+                    // Tell VS that we're about to change this file
+                    // This matters if the user has TFVC with server workpace (see #57)
+
+                    if (!vsHelper.CanEditFile(filePath))
+                    {
+                        Logger.Error($"VS or the user did no let us editing our file :/");
+                    }
+                    else
+                    {
+                        try
+                        {
+                            using (Stream fileStream = File.Open(filePath, FileMode.Create, FileAccess.Write))
+                            {
+                                ToolWindowProjectDataSerializer.Serialize(vm, fileStream);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Logger.Warn($"Failed to write to file '{filePath}' with error '{e}'.");
+                        }
+                    }
+                }
+            }
+            else if (File.Exists(filePath))
+            {
+                Logger.Info("Deleting json file because command list is empty but json-file exists.");
+
+                try
+                {
+                    File.Delete(filePath);
+                }
+                catch (Exception e)
+                {
+                    Logger.Warn($"Failed to delete file '{filePath}' with error '{e}'.");
+                }
+            }
+        }
+
+        private string FullFilenameForProjectJsonFileFromProject(IVsHierarchy project)
+        {
+            var userFilename = vsHelper.GetMSBuildPropertyValue(project, "SmartCmdArgJsonFile");
+
+            if (!string.IsNullOrEmpty(userFilename))
+            {
+                // It's recommended to use absolute paths for the json file in the first place...
+                userFilename = Path.GetFullPath(userFilename); // ... but make it absolute in any case.
+
+                Logger.Info($"'SmartCmdArgJsonFile' msbuild property present in project '{project.GetName()}' will use json file '{userFilename}'.");
+                return userFilename;
+            }
+            else
+            {
+                return FullFilenameForProjectJsonFileFromProjectPath(project.GetProjectDir(), project.GetName());
+            }
+        }
+
+        private string FullFilenameForProjectJsonFileFromProjectPath(string projectDir, string projectName)
+        {
+            string filename = $"{projectName}.args.json";
+            return Path.Combine(projectDir, filename);
+        }
+
+        private void FireFileStorageChanged(IVsHierarchy project)
+        {
+            FileStorageChanged?.Invoke(this, new FileStorageChangedEventArgs(project));
+        }
+
+        private void AttachFsWatcherToProject(IVsHierarchy project)
+        {
+            string unrealFilename = FullFilenameForProjectJsonFileFromProject(project);
+            string realProjectJsonFileFullName = SymbolicLinkUtils.GetRealPath(unrealFilename);
+            try
+            {
+                var projectJsonFileWatcher = new FileSystemWatcher();
+
+                projectJsonFileWatcher.Path = Path.GetDirectoryName(realProjectJsonFileFullName);
+                projectJsonFileWatcher.Filter = Path.GetFileName(realProjectJsonFileFullName);
+
+                projectJsonFileWatcher.EnableRaisingEvents = true;
+                projectFsWatchers.Add(project.GetGuid(), projectJsonFileWatcher);
+
+                projectJsonFileWatcher.Changed += (fsWatcher, args) => {
+                    Logger.Info($"SystemFileWatcher file Change '{args.FullPath}'");
+                    FireFileStorageChanged(project);
+                };
+                projectJsonFileWatcher.Created += (fsWatcher, args) => {
+                    Logger.Info($"SystemFileWatcher file Created '{args.FullPath}'");
+                    FireFileStorageChanged(project);
+                };
+                projectJsonFileWatcher.Renamed += (fsWatcher, args) =>
+                {
+                    Logger.Info($"FileWachter file Renamed '{args.FullPath}'. realProjectJsonFileFullName='{realProjectJsonFileFullName}'");
+                    if (realProjectJsonFileFullName == args.FullPath)
+                        FireFileStorageChanged(project);
+                };
+
+                Logger.Info($"Attached FileSystemWatcher to file '{realProjectJsonFileFullName}' for project '{project.GetName()}'.");
+            }
+            catch (Exception e)
+            {
+                Logger.Warn($"Failed to attach FileSystemWatcher to file '{realProjectJsonFileFullName}' for project '{project.GetName()}' with error '{e}'.");
+            }
+        }
+
+        private void DetachFsWatcherFromProject(IVsHierarchy project)
+        {
+            var guid = project.GetGuid();
+            if (projectFsWatchers.TryGetValue(guid, out FileSystemWatcher fsWatcher))
+            {
+                fsWatcher.Dispose();
+                projectFsWatchers.Remove(guid);
+                Logger.Info($"Detached FileSystemWatcher for project '{project.GetName()}'.");
+            }
+        }
+
+        private void DetachFsWatcherFromAllProjects()
+        {
+            foreach (var projectFsWatcher in projectFsWatchers)
+            {
+                projectFsWatcher.Value.Dispose();
+                Logger.Info($"Detached FileSystemWatcher for project '{projectFsWatcher.Key}'.");
+            }
+            projectFsWatchers.Clear();
+        }
+    }
+
+    class FileStorageChangedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The project for that the event was triggered
+        /// </summary>
+        public readonly IVsHierarchy Project;
+
+        public FileStorageChangedEventArgs(IVsHierarchy project)
+        {
+            Project = project;
+        }
+    }
+}
