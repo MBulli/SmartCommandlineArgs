67384c6012af2b6f28760a5d2cca071d0e7c1b5b
Irame <irame94@gmail.com>
2023-03-16 22:46:44 +0100

    Copy Comamnd Line now also has a Moniker icon

--- SmartCmdArgs/SmartCmdArgs.Shared/CustomMonikers.cs
+++ SmartCmdArgs/SmartCmdArgs.Shared/CustomMonikers.cs
@@ -10,5 +10,6 @@ namespace SmartCmdArgs
         private static readonly Guid ManifestGuid = new Guid("cafdbdaf-1847-4824-8957-a169d25d0cfb");
 
         public static ImageMoniker FoProjectNode => new ImageMoniker { Guid = ManifestGuid, Id = 0 };
+        public static ImageMoniker CopyCmdLine => new ImageMoniker { Guid = ManifestGuid, Id = 1 };
     }
 }
