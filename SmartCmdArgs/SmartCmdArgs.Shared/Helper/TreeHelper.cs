7244e90b01ea0f8d8c8062b98038dedbc2e04271
Irame <irame94@gmail.com>
2023-04-22 00:47:04 +0200

    added FindAncestor to TreeHelper

--- SmartCmdArgs/SmartCmdArgs.Shared/Helper/TreeHelper.cs
+++ SmartCmdArgs/SmartCmdArgs.Shared/Helper/TreeHelper.cs
@@ -10,7 +10,8 @@ namespace SmartCmdArgs.Helper
 {
     static class TreeHelper
     {
-        public static T FindVisualChild<T>(DependencyObject obj) where T : DependencyObject
+        public static T FindVisualChild<T>(DependencyObject obj)
+            where T : DependencyObject
         {
             for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
             {
@@ -30,7 +31,7 @@ namespace SmartCmdArgs.Helper
         }
 
         public static T FindAncestorOrSelf<T>(DependencyObject obj)
-        where T : DependencyObject
+            where T : DependencyObject
         {
             while (obj != null)
             {
@@ -43,7 +44,7 @@ namespace SmartCmdArgs.Helper
         }
 
         public static T FindAncestorOrSelf<T>(DependencyObject obj, string name)
-        where T : FrameworkElement
+            where T : FrameworkElement
         {
             while (obj != null)
             {
@@ -51,6 +52,24 @@ namespace SmartCmdArgs.Helper
                     return objTest;
                 obj = VisualTreeHelper.GetParent(obj);
             }
+            return null;
+        }
+
+        public static T FindAncestor<T>(DependencyObject obj, int skipLevels = 0)
+            where T : DependencyObject
+        {
+            if (skipLevels < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(skipLevels));
+            }
+
+            do {
+                obj = VisualTreeHelper.GetParent(obj);
+
+                if (obj is T objTest)
+                    return objTest;
+            } while (obj != null);
+
             return null;
         }
     }
