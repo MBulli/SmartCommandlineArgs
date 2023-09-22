670fa4023cb7e2837b7e81ec0ef61490ef4a6cd1
Felix <irame94@gmail.com>
2023-09-22 16:52:35 +0200

    made internals visible to tests

--- SmartCmdArgs/SmartCmdArgs17/Properties/AssemblyInfo.cs
+++ SmartCmdArgs/SmartCmdArgs17/Properties/AssemblyInfo.cs
@@ -31,3 +31,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.6.0.0")]
 [assembly: AssemblyFileVersion("2.6.0.0")]
+
+[assembly: InternalsVisibleTo("SmartCmdArgs17.Tests")]
