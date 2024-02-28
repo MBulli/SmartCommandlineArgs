648e0c4d5856f81e6d42a8f68538b6baa4c1233d
Irame <irame94@gmail.com>
2024-02-28 21:56:26 +0100

    added test project for dotnet8

---
+++ TestProjects/CS_DotNet/Program.cs
@@ -0,0 +1,13 @@
+﻿// Display command line arguments
+Console.WriteLine("Command Line Arguments:");
+foreach (var arg in args)
+{
+    Console.WriteLine(arg);
+}
+
+// Display environment variables
+Console.WriteLine("\nEnvironment Variables:");
+foreach (var key in Environment.GetEnvironmentVariables().Keys)
+{
+    Console.WriteLine($"{key} = {Environment.GetEnvironmentVariable(key?.ToString())}");
+}
