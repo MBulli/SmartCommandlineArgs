e8d7f0ab78065c4ea0591d0073a5303c255e1c1f
Felix <irame94@gmail.com>
2024-04-03 23:00:30 +0200

    fixed assembly version and file version

--- SmartCmdArgs/SmartCmdArgs16/Properties/AssemblyInfo.cs
+++ SmartCmdArgs/SmartCmdArgs16/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using SmartCmdArgs;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -29,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.6.0.1")]
-[assembly: AssemblyFileVersion("2.6.0.1")]
+[assembly: AssemblyVersion(Vsix.Version)]
+[assembly: AssemblyFileVersion(Vsix.Version)]
 
 [assembly: InternalsVisibleTo("SmartCmdArgs16.Tests")]
--- SmartCmdArgs/SmartCmdArgs17/Properties/AssemblyInfo.cs
+++ SmartCmdArgs/SmartCmdArgs17/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using SmartCmdArgs;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -29,8 +30,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.6.0.1")]
-[assembly: AssemblyFileVersion("2.6.0.1")]
+[assembly: AssemblyVersion(Vsix.Version)]
+[assembly: AssemblyFileVersion(Vsix.Version)]
 
 [assembly: InternalsVisibleTo("SmartCmdArgs17.Tests")]
 [assembly: InternalsVisibleTo("SmartCmdArgs.Tests")]
