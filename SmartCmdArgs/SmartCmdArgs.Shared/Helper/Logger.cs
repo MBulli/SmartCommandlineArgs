149315219f67a2aa704382516de485d83ef1aad1
Felix <irame94@gmail.com>
2025-01-26 19:50:22 +0100

    exceptions can be logged for errors

--- SmartCmdArgs/SmartCmdArgs.Shared/Helper/Logger.cs
+++ SmartCmdArgs/SmartCmdArgs.Shared/Helper/Logger.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using Microsoft.VisualStudio.Shell;
 
 namespace SmartCmdArgs.Helper
@@ -19,21 +20,29 @@ namespace SmartCmdArgs.Helper
             WriteToDebugConsole("Warning", message);
         }
 
-        public static void Error(string message)
+        public static void Error(string message, Exception ex = null)
         {
-            ActivityLog.TryLogError(LogSource, PrependThreadId(message));
-            WriteToDebugConsole("Error", message);
+            ActivityLog.TryLogError(LogSource, AppendException(PrependThreadId(message), ex));
+            WriteToDebugConsole("Error", message, ex);
         }
 
         [Conditional("DEBUG")]
-        private static void WriteToDebugConsole(string type, string message)
+        private static void WriteToDebugConsole(string type, string message, Exception ex = null)
         {
-            Debug.WriteLine($"{LogSource}[{System.Threading.Thread.CurrentThread.ManagedThreadId}]|{type}: {message}");
+            Debug.WriteLine($"{LogSource}[{System.Threading.Thread.CurrentThread.ManagedThreadId}]|{type}: {AppendException(message, ex)}");
         }
 
         private static string PrependThreadId(string message)
         {
             return $"[{System.Threading.Thread.CurrentThread.ManagedThreadId}] {message}";
+        }
+
+        private static string AppendException(string message, Exception ex)
+        {
+            if (ex == null)
+                return message;
+
+            return message + Environment.NewLine + ex.ToString();
         }
     }
 }
