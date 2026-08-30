267487b741dc91588b46cbe619bc58688f007be8
MBulli <mbulli135@gmail.com>
2026-08-30 14:48:49 +0200

    Changed the vsix's display name and description to include VS 2026 in the marketplace texts

--- SmartCmdArgs/SmartCmdArgs17/source.extension.cs
+++ SmartCmdArgs/SmartCmdArgs17/source.extension.cs
@@ -9,10 +9,10 @@ namespace SmartCmdArgs
     internal sealed partial class Vsix
     {
         public const string Id = "SmartCmdArgs17.6edd462d-d4d6-40b9-a7b4-451a6ce6c527";
-        public const string Name = "Smart Command Line Arguments VS2022";
-        public const string Description = @"A Visual Studio 2022 Extension which aims to provide a better UI to manage your command line arguments.";
+        public const string Name = "Smart Command Line Arguments VS 2026 and 2022";
+        public const string Description = @"A Visual Studio 2026 and 2022 Extension which aims to provide a better UI to manage your command line arguments.";
         public const string Language = "en-US";
-        public const string Version = "3.5.0";
+        public const string Version = "3.5.1";
         public const string Author = "Irame & MBulli";
         public const string Tags = "Commandline Command Line Arguments cmd project";
         public const bool IsPreview = false;
