9fade64beb3a505b16f922271525e4df4351454d
MBulli <mbulli135@gmail.com>
2026-08-30 14:34:47 +0200

    Bumped version to 3.5.0

--- SmartCmdArgs/SmartCmdArgs15/source.extension.cs
+++ SmartCmdArgs/SmartCmdArgs15/source.extension.cs
@@ -12,7 +12,7 @@ namespace SmartCmdArgs
         public const string Name = "Smart Command Line Arguments VS2017";
         public const string Description = @"A Visual Studio 2017 Extension which aims to provide a better UI to manage your command line arguments.";
         public const string Language = "en-US";
-        public const string Version = "3.4.0";
+        public const string Version = "3.5.0";
         public const string Author = "Irame & MBulli";
         public const string Tags = "Commandline Command Line Arguments cmd project";
         public const bool IsPreview = false;
